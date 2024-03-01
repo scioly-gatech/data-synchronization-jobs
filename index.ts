3fe280b323e32b6e2b044d8c9e1dc471af23c6af
rzhang630 <rzhang630@gatech.edu>
2024-02-29 23:13:44 -0500

    Added JSON file creation

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { drive } from "@googleapis/drive";
 import fs from "fs";
 import path from "path";
+import { getH1GroupsFromHTML } from "./utils";
 
 const driveClient = drive({
   version: "v3",
@@ -16,12 +17,19 @@ async function main() {
     fileId: "1Vnjq-zAN3x7LlF2YKXY5EmpDy5hn_R_0yhbFvusNeXM",
     mimeType: "text/html",
   });
-  if (isString(file.data)) {
-    if (!fs.existsSync("./data")) {
-      fs.mkdirSync("./data");
-    }
-    fs.writeFileSync(path.join("data", "tournament-updates.html"), file.data);
+  if (!isString(file.data)) {
+    return;
   }
+  if (!fs.existsSync("./data")) {
+    fs.mkdirSync("./data");
+  }
+  // Write the HTML document of announcements
+  fs.writeFileSync(path.join("data", "tournament-updates.html"), file.data);
+
+  const parsedAnnouncements = getH1GroupsFromHTML(file.data);
+  
+  // Write the JSON file of announcements
+  fs.writeFileSync(path.join("data", "tournament-updates.json"), JSON.stringify(parsedAnnouncements))
 }
 
 main();
